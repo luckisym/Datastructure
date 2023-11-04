2b10a7ac412aade718a1b9aa600890e33fca4323
saleeman <saleemaany@gmail.com>
2023-11-04 20:16:47 +0100

    Queue en stack geimplementeerd

--- maze_solver/maze_solver/queue.c
+++ maze_solver/maze_solver/queue.c
@@ -4,37 +4,119 @@
 #include "queue.h"
 
 struct queue {
-    /* ... SOME CODE MISSING HERE ... */
+    int front;
+    int rear; 
+    int *data; 
+    size_t capacity;
+    int push_count; 
+    int pop_count; 
 };
 
 struct queue *queue_init(size_t capacity) {
-    /* ... SOME CODE MISSING HERE ... */
+    struct queue *q = malloc(sizeof(struct queue));
+
+    if (q == NULL) {
+        free(q);
+        perror("Error: "); // TODO: goede error message
+        return NULL; 
+    }
+
+    q->front = -1; 
+    q->rear = -1; 
+    q->data = (int *)malloc(sizeof(int) * capacity);
+    q->capacity = capacity;
+
+    if (q->data == NULL) {
+        free(q->data);
+        perror("Error: "); // TODO: goede error message
+        return NULL; 
+    }
+    
+    return q;
 }
 
 void queue_cleanup(struct queue *q) {
-    /* ... SOME CODE MISSING HERE ... */
+    if (q == NULL) {
+        return;  
+    }
+
+    free(q->data); 
+    free(q); 
 }
 
 void queue_stats(const struct queue *q) {
-    /* ... SOME CODE MISSING HERE ... */
+    
 }
 
 int queue_push(struct queue *q, int e) {
-    /* ... SOME CODE MISSING HERE ... */
+    if (q == NULL || ((q->rear + 1) % (int) q->capacity) == q->front) {
+        return 1;  
+    }
+
+    if (queue_empty(q)) {
+        q->rear = 0; 
+        q->front = 0; 
+
+        q->data[q->rear] = e; 
+    } else {
+        q->rear = (q->rear + 1) % (int) q->capacity; 
+        q->data[q->rear] = e; 
+    }
+    
+    q->push_count++; 
+
+    return 0;
 }
 
 int queue_pop(struct queue *q) {
-    /* ... SOME CODE MISSING HERE ... */
+    if (q == NULL || queue_empty(q)) {
+        return -1;  
+    }
+
+    int item; 
+    
+    if (q->rear == q->front) {
+        item = q->data[q->front]; 
+        q->rear = -1; 
+        q->front = -1; 
+    } else {
+        item = q->data[q->front]; 
+        q->front = (q->front + 1) % (int) q->capacity; 
+    }
+
+    q->pop_count++; 
+    
+    return item; 
 }
 
 int queue_peek(const struct queue *q) {
-    /* ... SOME CODE MISSING HERE ... */
+    if (q == NULL || queue_empty(q)) {
+        return -1;  
+    }
+    
+    return q->data[q->front]; 
 }
 
 int queue_empty(const struct queue *q) {
-    /* ... SOME CODE MISSING HERE ... */
+    if (q == NULL) {
+        return -1;  
+    }
+
+    return (q->front == -1 && q->rear == -1) ? 1 : 0;
 }
 
 size_t queue_size(const struct queue *q) {
-    /* ... SOME CODE MISSING HERE ... */
+    if (q == NULL) {
+        return (size_t) -1;  
+    }
+
+    size_t size; 
+
+    if (q->front > q->rear) {
+        size = (size_t) ((q->front - q->rear) + 1); 
+    } else if (q->rear > q->front) {
+        size = (size_t) ((q->rear - q->front) + 1); 
+    }
+
+    return size; 
 }
--- maze_solver/maze_solver/stack.c
+++ maze_solver/maze_solver/stack.c
@@ -4,37 +4,108 @@
 #include "stack.h"
 
 struct stack {
-    /* ... SOME CODE MISSING HERE ... */
+    int pos;
+    int *data;
+    size_t capacity;
+    int push_count; 
+    int pop_count; 
 };
 
 struct stack *stack_init(size_t capacity) {
-    /* ... SOME CODE MISSING HERE ... */
+    struct stack *s = malloc(sizeof(struct stack));
+
+    if (s == NULL) {
+        free(s);
+        perror("Error: "); // TODO: goede error message
+        return NULL; 
+    }
+
+    s->pos = -1; 
+    s->data = (int *)malloc(sizeof(int) * capacity);
+    s->capacity = capacity;
+    s->pop_count = 0; 
+    s->push_count = 0; 
+
+    if (s->data == NULL) {
+        free(s->data);
+        perror("Error: "); // TODO: goede error message
+        return NULL; 
+    }
+    
+    return s;
 }
 
 void stack_cleanup(struct stack *s) {
-    /* ... SOME CODE MISSING HERE ... */
+    if (s == NULL) {
+        return;  
+    }
+
+    free(s->data);
+    free(s); 
 }
 
 void stack_stats(const struct stack *s) {
-    /* ... SOME CODE MISSING HERE ... */
+    if (s == NULL) {
+        return;  
+    }
+
+    fprintf(stderr, "stats: %d %d %ld", s->push_count, s->pop_count, s->capacity);
 }
 
-int stack_push(struct stack *s, int c) {
-    /* ... SOME CODE MISSING HERE ... */
+int stack_push(struct stack *s, int c) { 
+    if (s == NULL) {
+        return 1;  
+    }
+
+    if (s->pos == (int) s->capacity - 1) {
+        return 1;
+    }
+
+    s->pos++;
+    s->data[s->pos] = c;
+    s->push_count++; 
+    return 0;
 }
 
 int stack_pop(struct stack *s) {
-    /* ... SOME CODE MISSING HERE ... */
+    if (s == NULL) {
+        return -1;  
+    }
+
+    if (stack_empty(s)) {
+        return -1;
+    }
+
+    int item = s->data[s->pos];
+    s->pos--;
+    s->pop_count++; 
+    
+    return item; 
 }
 
 int stack_peek(const struct stack *s) {
-    /* ... SOME CODE MISSING HERE ... */
+    if (s == NULL) {
+        return -1;  
+    }
+
+    if (stack_empty(s)) {
+        return -1;
+    }
+
+    return s->data[s->pos]; 
 }
 
 int stack_empty(const struct stack *s) {
-    /* ... SOME CODE MISSING HERE ... */
+    if (s == NULL) {
+        return -1;  
+    }
+
+    return (s->pos == -1) ? 1 : 0; 
 }
 
 size_t stack_size(const struct stack *s) {
-    /* ... SOME CODE MISSING HERE ... */
+    if (s == NULL) {
+        return (size_t) -1;  
+    }
+    return (size_t) s->pos + 1;
 }
