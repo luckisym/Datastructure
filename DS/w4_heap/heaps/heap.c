b8c2b9fa004647c3d9c5aa79fc27fcf3f163005b
saleeman <saleemaany@gmail.com>
2023-12-09 08:49:28 +0100

    heaps af

--- DS/w4_heap/heaps/heap.c
+++ DS/w4_heap/heaps/heap.c
@@ -1,3 +1,14 @@
+/**
+ * Auteur: Saleeman Mahamud
+ * Studentennummer: 14932458
+ * Study: Computer Science
+ *
+ * Implementation of a priority queue using a min heap (where the value of each node is less than
+ * or equal to the values of its children).
+ * The priority queue supports insertion and removal of elements with priorities, where the element
+ * with the highest priority is dequeued first. The comparison function for priorities is provided
+ * during initialization of the priority queue.
+*/
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -5,14 +16,23 @@
 #include "array.h"
 #include "prioq.h"
 
+#define ARRAY_INIT_SIZE 100
+
 static struct heap *heap_init(int (*compare)(const void *, const void *)) {
-    /* ... CODE MISSING HERE .... */
+    struct heap *h = (struct heap *)malloc(sizeof(struct heap));
+    if (h == NULL) {
+        return NULL;
+    }
 
-    /* Store the function pointer compare argument in the struct here.
-       If the struct pointer is called 'h' the assignment is:
-       h->compare = compare; */
+    h->array = array_init(ARRAY_INIT_SIZE);
+    if (h->array == NULL) {
+        free(h);
+        return NULL;
+    }
 
-    /* ... CODE MISSING HERE .... */
+    h->compare = compare;
+
+    return h;
 }
 
 prioq *prioq_init(int (*compare)(const void *, const void *)) {
@@ -20,32 +40,123 @@ prioq *prioq_init(int (*compare)(const void *, const void *)) {
 }
 
 long int prioq_size(const prioq *q) {
-    /* ... CODE MISSING HERE .... */
+    return (q != NULL) ? array_size(q->array) : -1;
 }
 
 static int heap_cleanup(struct heap *h, void free_func(void *)) {
-    /* ... CODE MISSING HERE .... */
+    if (h == NULL) {
+        return 1; // return 1 on error
+    }
+
+    array_cleanup(h->array, free_func);
+    free(h);
+
+    return 0;
 }
 
 int prioq_cleanup(prioq *q, void free_func(void *)) {
-    /* ... CODE MISSING HERE .... */
+    return heap_cleanup(q, free_func);
 }
 
 static int heap_insert(struct heap *h, void *p) {
-    /* ... CODE MISSING HERE .... */
+    if (h == NULL || p == NULL) {
+        return 1; // return 1 on error
+    }
 
-    /* To compare items in the heap these heaps functions can use the stored
-       compare function pointer in the following way:
-       if (h->compare(arg1, arg1)) { ...  */
+    array_append(h->array, p);
+    long i = array_size(h->array) - 1;
+    long parent_i = (i - 1) / 2;
+    void *parent = array_get(h->array, parent_i);
+
+    while (i > 0) {
+        if (h->compare(p, parent) > 0) {
+            break;
+        }
+
+        array_set(h->array, i, parent);
+        array_set(h->array, parent_i, p);
+
+        i = parent_i;
+        parent_i = (i - 1) / 2;
+        parent = array_get(h->array, parent_i);
+    }
+
+    return 0;
 }
 
 int prioq_insert(prioq *q, void *p) {
     return heap_insert(q, p);
 }
 
-static void *heap_pop(struct heap *h) {
-    /* ... CODE MISSING HERE .... */
+/**
+ * Perform heap sort on a given heap.
+ *
+ * This function implements the heap sort algorithm to sort the elements
+ * in the provided heap in ascending order to keep/ make it a min heap where the minimum element is
+ * at the root.
+ *
+ * in:
+ *  - h: heap to be sorted
+ *
+ * side effects:
+ *  - changes the heap layout
+ */
+static void heap_sort(struct heap *h) {
+    if (h == NULL) {
+        return;
+    }
+
+    long size = array_size(h->array);
+    long i = 0;
+
+    while (i < size / 2) {
+        long left_i = 2 * i + 1;
+        long right_i = 2 * i + 2;
+        long parent_i = i;
+
+        void *left_child = (left_i < size) ? array_get(h->array, left_i) : NULL;
+        void *right_child = (right_i < size) ? array_get(h->array, right_i) : NULL;
+        void *parent = array_get(h->array, parent_i);
+
+        if (left_child != NULL && h->compare(left_child, parent) < 0) {
+            parent_i = left_i;
+            parent = array_get(h->array, left_i);
+        }
+
+        if (right_child != NULL && h->compare(right_child, parent) < 0) {
+            parent_i = right_i;
+        }
+
+        if (parent_i != i) {
+            void *temp = array_get(h->array, i);
+
+            array_set(h->array, i, array_get(h->array, parent_i));
+            array_set(h->array, parent_i, temp);
+
+            i = parent_i;
+        } else {
+            break;
+        }
+    }
+
 }
+
+static void *heap_pop(struct heap *h) {
+    if (h == NULL || array_size(h->array) == 0) {
+        return NULL;
+    }
+
+    long firs_index = 0;
+    void *first_elem = array_get(h->array, firs_index);
+    void *last_elem = array_pop(h->array);
+
+    array_set(h->array, firs_index, last_elem);
+    heap_sort(h);
+
+    return first_elem;
+}
+
+
 
 void *prioq_pop(prioq *q) {
     return heap_pop(q);
--- DS/w4_heap/heaps/main.c
+++ DS/w4_heap/heaps/main.c
@@ -1,9 +1,20 @@
+/**
+ * Auteur: Saleeman Mahamud
+ * Studentennummer: 14932458
+ * Study: Computer Science
+ *
+ * This program simulates a doctor's office where patients are registered, treated, and released.
+ * It maintains a priority queue of patients based on either their names or ages.
+ * Additional features include sorting patients by age and managing the treatment duration.
+ */
+
 #include <fcntl.h>
 #include <getopt.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <stdbool.h>
 
 #include "prioq.h"
 
@@ -23,21 +34,256 @@ static int parse_options(struct config *cfg, int argc, char *argv[]);
 
 typedef struct {
     char *name;
-    /* ... CODE MISSING HERE .... */
+    int age;
+    int duration;
+    bool keep_track_of_duration;
 } patient_t;
+
+enum return_type {
+    CONTINUE,
+    BREAK,
+    SUCCES,
+    FAILURE
+};
+
+/**Creates a new patient with the given attributes, makes space for it and returns the pointer to
+ * the given space.
+ *
+ * in:
+ *  - name: name of the patient
+ *  - age: age of the patient
+ *  - duration: the duration of the patient's treatment
+ *
+ * out:
+ *  - a pointer to new patient
+ *
+ * side effects:
+ *  - allocates memory for the new patient
+*/
+static patient_t *patient_init(char *name, int age, int duration) {
+    if (name == NULL) {
+        return NULL;
+    }
+
+    patient_t *p = (patient_t *)malloc(sizeof(patient_t));
+    if (p == NULL) {
+        return NULL;
+    }
+
+    p->name = name;
+    p->age = age;
+    p->duration = duration;
+    p->keep_track_of_duration = true;
+
+    if (duration <= 0) {
+        p->keep_track_of_duration = false;
+    }
+
+    return p;
+}
+
+/**Cleans up the allocated memory for a patient
+ *
+ * in:
+ * - obj: the patient to be freed
+*/
+static void patient_cleanup(void *obj) {
+    if (obj == NULL) {
+        return;
+    }
+
+    patient_t *p = (patient_t *)obj;
+    free(p->name);
+    free(p);
+}
+
+/**Prints the remaining patients in the priority queue while freeing them.
+ *
+ * in:
+ *  - q: the priority queue where the remaining patients are
+ *
+ * side effects:
+ *  - prints the remaing patients to the terminal
+*/
+static void print_remaining_patients(prioq *q) {
+    patient_t *curr = NULL;
+
+    while (prioq_size(q) > 0) {
+        curr = (patient_t *) prioq_pop(q);
+        printf("%s\n", curr->name);
+        patient_cleanup(curr);
+    }
+}
+
+/** This function uses strtok to tokenize the input string and extract
+ * patient information including name, age, and optional duration.
+ *
+ * in:
+ *  - token: Pointer to a string pointer to store the current token.
+ *  - name_cpy: Pointer to a string pointer to store the copied name.
+ *  - age: Pointer to an integer to store the extracted age.
+ *  - duration: Pointer to an integer to store the extracted duration.
+ *
+ * out:
+ * An enum representing the result of the operation:
+ *  - CONTINUE: continue in the while loop where the function is called
+ *  - BREAK: break in the while loop where the function is called
+ *  - FAILURE: there was an error
+ *  - SUCCES: there was no error
+ *
+ * side effects:
+ *  - allocates memory for name_cpy
+ */
+static enum return_type get_patient_info(char **name_cpy, int *age, int *duration) {
+    char *token = strtok(buf, " \n");
+    if (token == NULL) {
+        return CONTINUE;
+    }
+
+    if (strcmp(token, ".") == 0) {
+        return BREAK;
+    }
+
+    *name_cpy = (char *)malloc(sizeof(char) * (strlen(token) + 1));
+    if (*name_cpy == NULL) {
+        return FAILURE;
+    }
+
+    strcpy(*name_cpy, token);
+
+    token = strtok(NULL, " \n");
+    if (token == NULL) {
+        free(*name_cpy);
+        return CONTINUE;
+    }
+
+    *age = atoi(token);
+
+    token = strtok(NULL, " \n");
+    if (token != NULL) {
+        *duration = atoi(token);
+    }
+
+    return SUCCES;
+}
+
+/**This function reads patient information from the standard input and registers
+ * them into the specified queue. The patient information includes name,
+ * age, and an optional treatment duration. The function continues reading patient
+ * data until a period (.) is encountered.
+ *
+ * in:
+ *  - queue: The priority queue to save the new patients.
+ *
+ * out:
+ *  - EXIT_SUCCESS: The function executed successfully
+ *  - EXIT_FAILURE: There was an error
+ */
+static int register_new_patients(prioq *queue) {
+    char *name_cpy;
+    int duration = 0, age = 0;
+
+    while (1) {
+        char *s = fgets(buf, BUF_SIZE, stdin);
+        if (s == NULL) {
+            fprintf(stderr, "Unexpected end of file. exiting\n");
+            prioq_cleanup(queue, patient_cleanup);
+            return EXIT_FAILURE;
+        }
+
+        int return_t = get_patient_info(&name_cpy, &age, &duration);
+        if (return_t == BREAK) {
+            break;
+        } else if (return_t == CONTINUE) {
+            continue;
+        } else if(return_t == FAILURE) {
+            return EXIT_FAILURE;
+        }
+
+        patient_t *patient = patient_init(name_cpy, age, duration);
+        if (patient == NULL) {
+            prioq_cleanup(queue, patient_cleanup);
+            return EXIT_FAILURE;
+        }
+
+        prioq_insert(queue, patient);
+    }
+
+    return EXIT_SUCCESS;
+}
+
+/**Process patient, if no patient is currently in treatment and the priority queue is not empty,
+ * a patient is dequeued and their name is printed(if the duration isnt a factor).
+ * If the duration is a factor: if a patient is in treatment and the elapsed time equals the
+ * patient's duration, the patient's name is printed.
+ *
+ * in:
+ *  - queue: The priority queue containing patient information.
+ *  - t_elapsed: Pointer to the elapsed time.
+ *  - patient_duration: Pointer to the duration of the patient in treatment.
+ *  - name_curr_p: Pointer to a string to store the name of the patient in treatment.
+ *
+ * side effects:
+ *  - allocates memory for name_curr_p if there no patient getting treated
+ */
+void process_patient(prioq *queue, int *t_elapsed, int *patient_duration, char **name_curr_p) {
+    if (*name_curr_p == NULL && prioq_size(queue) > 0) {
+        patient_t *curr_patient = (patient_t *)prioq_pop(queue);
+
+        if (!curr_patient->keep_track_of_duration) {
+            printf("%s\n", curr_patient->name);
+        } else {
+            *patient_duration = curr_patient->duration;
+            *name_curr_p = (char *)malloc(sizeof(char) * (strlen(curr_patient->name) + 1));
+
+            strcpy(*name_curr_p, curr_patient->name);
+        }
+
+        patient_cleanup(curr_patient);
+    }
+
+    if (*name_curr_p != NULL && *t_elapsed == *patient_duration) {
+        printf("%s\n", *name_curr_p);
+        free(*name_curr_p);
+
+        *name_curr_p = NULL;
+        *t_elapsed = 1;
+        *patient_duration = 0;
+    }
+}
 
 static int compare_patient_name(const void *a, const void *b) {
     return strcmp(((const patient_t *) a)->name, ((const patient_t *) b)->name);
 }
 
+/**This function compares the age of patient 'a' and 'b'. If their ages are equal,
+ * it further compares their names using the compare_patient_name function.
+ *
+ * in:
+ *  - a: patient a
+ *  - b: patient b
+ *
+ * out:
+ *  - 0 if ages are equal, and names are also equal.
+ *  - A positive value if the age of 'a' is greater than the age of 'b'.
+ *  - A negative value if the age of 'a' is less than the age of 'b'.
+*/
 static int compare_patient_age(const void *a, const void *b) {
-    /* ... CODE MISSING HERE .... */
+    int diffrence = ((const patient_t *)a)->age - ((const patient_t *)b)->age;
+    return (diffrence == 0) ? compare_patient_name(a, b) : diffrence;
 }
 
+/**This function initializes a patient priority queue based on the program configuration,
+ * processes patient registrations, tracks elapsed time, and manages patient treatment cycles.
+ * And patient name is outputted after the treatment, the remaining patients after 10 iterations
+ * are printed aswell.
+*/
 int main(int argc, char *argv[]) {
-    char *token, *name_cpy;
+    char *name_curr_p = NULL;
     prioq *queue;
     struct config cfg;
+    int t_elapsed = 1;
+    int patient_duration = -1;
 
     if (parse_options(&cfg, argc, argv) != 0) {
         return EXIT_FAILURE;
@@ -48,32 +294,36 @@ int main(int argc, char *argv[]) {
     } else {
         queue = prioq_init(&compare_patient_name);
     }
-    /* ... CODE MISSING HERE .... */
+
+    if (queue == NULL) {
+        return EXIT_FAILURE;
+    }
 
     for (int iterations = 0;;) {
-        while (1) {
-            char *s = fgets(buf, BUF_SIZE, stdin);
-            if (s == NULL) {
-                fprintf(stderr, "Unexpected end of file. exiting\n");
-                /* ... CODE MISSING HERE .... */
-                return EXIT_FAILURE;
-            }
-
-            /* ... CODE MISSING HERE .... */
+        if (register_new_patients(queue)) {
+            return EXIT_FAILURE;
         }
 
-        printf(".\n"); /* End turn. */
+        process_patient(queue, &t_elapsed, &patient_duration, &name_curr_p);
+
+        if (name_curr_p != NULL) {
+            t_elapsed++;
+        }
+
+        printf(".\n");
 
         if (++iterations == 10) {
+            if (name_curr_p != NULL) {
+                printf("%s\n", name_curr_p);
+                free(name_curr_p);
+            }
 
-            /* ... CODE MISSING HERE .... */
-
+            print_remaining_patients(queue);
             break;
         }
     }
 
-    /* ... CODE MISSING HERE .... */
-
+    prioq_cleanup(queue, patient_cleanup);
     return EXIT_SUCCESS;
 }
 
